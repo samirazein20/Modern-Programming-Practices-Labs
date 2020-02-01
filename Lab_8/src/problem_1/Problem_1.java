4f3a92558eafcaf0ffbaa8c53ed0ee9f90fed1ba
Samira Zein <samirazein20@gmail.com>
2020-01-31 23:56:07 -0600

    Commented out code in Problem_1.java code that was not part of the solution.

--- Lab_8/src/problem_1/Problem_1.java
+++ Lab_8/src/problem_1/Problem_1.java
@@ -1,11 +1,6 @@
 package problem_1;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.function.BiFunction;
-import java.util.function.BiPredicate;
-import java.util.function.Function;
-import java.util.function.Predicate;
 
 public class Problem_1 {
     public static void main(String[] args) {
@@ -13,16 +8,16 @@ public class Problem_1 {
         Double result = ex.apply(10.0, 2.0);
         System.out.println("Result : "+result);
 
-        List<Integer> list = new ArrayList<>();
-        list.add(0);
-        list.add(1);
-        list.add(2);
-        System.out.println(myPredicate.test(list, 9));
-        System.out.println(myPredicate.test(list, 1));
+//        List<Integer> list = new ArrayList<>();
+//        list.add(0);
+//        list.add(1);
+//        list.add(2);
+//        System.out.println(myPredicate.test(list, 9));
+//        System.out.println(myPredicate.test(list, 1));
 
     }
 
-    BiFunction<Integer, Integer, Boolean> myfunction = (x, y) -> x*x == y;
-
-    static BiPredicate<List<Integer>, Integer> myPredicate = (list, element) -> list.contains(element);
+//    BiFunction<Integer, Integer, Boolean> myfunction = (x, y) -> x*x == y;
+//
+//    static BiPredicate<List<Integer>, Integer> myPredicate = (list, element) -> list.contains(element);
 }
